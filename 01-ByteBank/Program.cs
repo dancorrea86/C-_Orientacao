eee54df3d25834ad27e414d03fe9c5720ffd7937
dancorrea86 <danielmacielcorrea@gmail.com>
2022-06-12 21:34:06 -0300

    Feature 1 - Final da aula

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.ReadLine();
+        }
+    }
+}
